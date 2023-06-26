8e6973a4252a3a41da32b913d74827d46681ef3d
Manuel Wudka-Robles <mwudka@gmail.com>
2023-06-26 13:55:16 -0700

    Streaming method works in nodejs
    The completeStream method now passes the `fetch` from `cross-fetch` 
    to the fetchEventSource. This makes it work properly on nodejs or other 
    platforms that don't natively support fetch.

--- src/index.ts
+++ src/index.ts
@@ -93,6 +93,7 @@ export class Client {
       });
 
       fetchEventSource(`${this.apiUrl}/v1/complete`, {
+        fetch: fetch,
         method: "POST",
         headers: {
           Accept: "application/json",
